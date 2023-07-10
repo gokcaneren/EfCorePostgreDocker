08627e932fe1b1b14dbb147bbd4a64339c044753
gokcaneren <gokcann01@gmail.com>
2023-07-10 11:01:22 +0300

    docker-compose added to project

--- EfCorePostgreDocker.API/obj/Debug/net7.0/EfCorePostgreDocker.API.AssemblyInfo.cs
+++ EfCorePostgreDocker.API/obj/Debug/net7.0/EfCorePostgreDocker.API.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("7c57843c-3cb5-44b7-afc8-06310ffa242d")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("EfCorePostgreDocker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
